0df975f2aeb3d450ce8f7df5292559e49416caa9
mversionpenny <margot.selosse@gmail.com>
2017-03-01 20:08:24 +0100

    finished code with LR

--- lr.R
+++ lr.R
@@ -6,23 +6,20 @@ install.packages("glmnet")
 library("glmnet")
 library(caret)
 
+load("bows.RData")
+
 x.train <- as.matrix(bow.train[,-1])
 y.train <- as.vector(bow.train[,1])
 x.val <- as.matrix(bow.val[,-1])
 y.val <- as.vector(bow.val[,1])
-glmnet.fit <- glmnet(x=x.train,y=y.train, family = "multinomial", alpha = 0)  
-glmnet.pred <- predict(glmnet.fit, newx = x.val, type="class")
-matrix <- confusionMatrix( glmnet.pred[,50], y.val)
-matrix$overall[1]
-dim(glmnet.pred)
+x.test1 <- as.matrix(bow.test1[,-1])
+y.test1 <- as.vector(bow.test1[,1])
+x.test2 <- as.matrix(bow.test2[,-1])
+y.test2 <- as.vector(bow.test2[,1])
 
 
 
-findBest <- function(){
-  x.train <- as.matrix(bow.train[,-1])
-  y.train <- as.vector(bow.train[,1])
-  x.val <- as.matrix(bow.val[,-1])
-  y.val <- as.vector(bow.val[,1])
+findBest <- function(x.train, y.train, x.val, y.val){
   
   sequence <-seq(0,1,0.1)
   bestAlpha <- 0
@@ -45,3 +42,20 @@ findBest <- function(){
   }
   return(resultRet)
 }
+
+# do not run 
+# lrParams <- findBest()
+load(file= "lrParams.RData")
+lrParams$alpha
+x.train <- rbind(x.train, x.val)
+y.train <- c(y.train, y.val)
+glmnet.fit <- glmnet(x=x.train, y=y.train,
+                     family="multinomial", lambda = lrParams$lambda, 
+                     alpha=lrParams$alpha)
+glmnet.pred1 <- predict(glmnet.fit, newx = x.test1, type="class")
+lrTest1 <- confusionMatrix( glmnet.pred1, y.test1)
+save(lrTest1, file="lrTest1.RData")
+
+glmnet.pred2 <- predict(glmnet.fit, newx = x.test2, type="class")
+lrTest2 <- confusionMatrix(glmnet.pred2, y.test2)
+save(lrTest2, file="lrTest2.RData")
